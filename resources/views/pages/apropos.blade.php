165c0abac2f3042d0e9ee9b036632fa4e05e61a8
Cheikh Tidiane KHOULE <ctkhoule@gmail.com>
2021-04-18 15:23:16 +0000

    Update app, apropos and home pages

--- resources/views/pages/apropos.blade.php
+++ resources/views/pages/apropos.blade.php
@@ -1,9 +1,9 @@
 @extends('app')
 
+@section('title', 'A propos | Hello City')
+  
+
 @section('content')
-
-<p>Développé par Cheikh TK &hearts;</p>
-
-<p><a href="/">Retour à la page d'accueil</a></p>
-
+  <p>Développé par Cheikh TK &hearts;</p>
+  <p><a href="/">Retour à la page d'accueil</a></p>
 @endsection
--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,15 +1,12 @@
 @extends('app')
 
+@section('title', 'Hello City')
+
 @section('content')
-
-<h1>Hello from Dakar!</h1>
-
-<p>Il est en ce moment : {{ date('h:i A') }} UTC</p>
-
+  <h1>Hello from Dakar!</h1>
+  <p>Il est en ce moment : {{ date('h:i A') }} UTC</p>
 @endsection
 
 @section('linkabout')
-
   &middot; <a href="/a-propos">A propos</a> </p>
-
 @endsection
