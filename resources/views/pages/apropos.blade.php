bfc91ae0899cbc899772cbf6f61042421734affb
Cheikh Tidiane KHOULE <ctkhoule@gmail.com>
2021-04-19 15:08:19 +0000

    Add images

--- resources/views/pages/apropos.blade.php
+++ resources/views/pages/apropos.blade.php
@@ -4,6 +4,8 @@
   
 
 @section('content')
+  <img src="/images/profil.jpg" alt="Profile Cheikh TK" width="150" height="150">
+
   <p>Développé par Cheikh TK &hearts;</p>
   <p><a href="{{ route('home') }}">Retour à la page d'accueil</a></p>
 @endsection
--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -3,6 +3,8 @@
 @section('title')
 
 @section('content')
+  <img src="/images/sn.png" alt="Drapeau du Senegal" width="150" height="150">
+
   <h1>Hello from Dakar!</h1>
   <p>Il est en ce moment : {{ date('h:i A') }} UTC</p>
 @endsection
