08474137cc089b8c4a6fca7868d430a33d8c2e03
Cheikh Tidiane KHOULE <ctkhoule@gmail.com>
2021-04-19 15:31:46 +0000

    Use asset function to load images

--- resources/views/pages/apropos.blade.php
+++ resources/views/pages/apropos.blade.php
@@ -4,7 +4,7 @@
   
 
 @section('content')
-  <img src="/images/profil.jpg" alt="Profile Cheikh TK" width="150" height="150">
+  <img src="{{ asset('images/profil.jpg') }}" alt="Profile Cheikh TK" width="150" height="150">
 
   <p>Développé par Cheikh TK &hearts;</p>
   <p><a href="{{ route('home') }}">Retour à la page d'accueil</a></p>
